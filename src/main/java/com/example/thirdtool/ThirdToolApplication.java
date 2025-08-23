19359e28631ebd6122c4e7ffa53a7755ccb9598f
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-08-23 20:08:20 +0900

    First commit

---
+++ src/main/java/com/example/thirdtool/ThirdToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdToolApplication.class, args);
+    }
+
+}

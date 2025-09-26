6a9da9b751e20a118adac3d22c4489a025284411
이성혁 <danial2569@gmail.com>
2025-09-27 00:10:40 +0900

    TTB-59 덱 검색 자동 완성 기능

--- src/main/java/com/example/thirdtool/Deck/presentation/controller/DeckController.java
+++ src/main/java/com/example/thirdtool/Deck/presentation/controller/DeckController.java
@@ -78,7 +78,7 @@ public class DeckController {
     public ResponseEntity<List<DeckResponseDto>> getDecksByTag(@RequestParam(required = false) List<Long> tagIds) {
         Long userId = 1L; // TODO: 임시 사용자, 나중에 JWT/리졸버로 대체
         List<DeckResponseDto> decks = deckService.findDecksByTagId(userId, tagIds);
-        return ResponseEntity.ok(decks); 
+        return ResponseEntity.ok(decks);
     }
 
     //덱 이름 자동완성
@@ -86,7 +86,7 @@ public class DeckController {
     public ResponseEntity<List<DeckSearchDto>> autoCompleteDeckNames(@RequestParam String keyword, @RequestParam(defaultValue = "public") String scope){
         Long userId = 1L;
         List<DeckSearchDto> suggestions = deckService.getAutocompleteDeckNames(keyword, scope, userId);
-        return ResponseEntity.ok(suggestions);
+        return ResponseEntity.ok(suggestions); 
     }
 
     //덱 공유하기(공개하기)

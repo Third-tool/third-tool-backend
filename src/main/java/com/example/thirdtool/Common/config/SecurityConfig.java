1f6441effea3aa6b624336bb2424e1b25641e3f1
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-10-18 15:03:12 +0900

    localhost 3000 임시변환

--- src/main/java/com/example/thirdtool/Common/config/SecurityConfig.java
+++ src/main/java/com/example/thirdtool/Common/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080","http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);

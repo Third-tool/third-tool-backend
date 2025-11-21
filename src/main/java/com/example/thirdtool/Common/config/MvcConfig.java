e44c5378ea01947c2ddb9cb8f57ee10911c78cdd
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-11-21 15:55:01 +0900

    cors 문제 코드 추가

--- src/main/java/com/example/thirdtool/Common/config/MvcConfig.java
+++ src/main/java/com/example/thirdtool/Common/config/MvcConfig.java
@@ -11,7 +11,13 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173","http://localhost:8080")
+                    .allowedOrigins(
+                            "http://localhost:5173",
+                            "http://localhost:8080",
+                            "http://localhost:3000",
+                            "https://thirdstool.com",       // ✅ FE 배포 도메인
+                            "https://www.thirdstool.com" // www 쓰면 추가
+                                   )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowCredentials(true)
                     .allowedHeaders("*")

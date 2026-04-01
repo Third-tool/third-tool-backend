aee7bd19cacb5dd6aa10c125bba65a4124295abf
junseong kim <67666411+js-kim-arc@users.noreply.github.com>
2026-04-01 22:14:03 +0900

    fix(ReviewSessionException):상속 관계 buisinessException으로 수정

--- src/main/java/com/example/thirdtool/Review/domain/exception/ReviewSessionException.java
+++ src/main/java/com/example/thirdtool/Review/domain/exception/ReviewSessionException.java
@@ -1,13 +1,24 @@
 package com.example.thirdtool.Review.domain.exception;
 
-public class ReviewSessionException extends RuntimeException {
+import com.example.thirdtool.Common.Exception.BusinessException;
+import com.example.thirdtool.Common.Exception.ErrorCode.ErrorCode;
 
-    public ReviewSessionException(String message) {
-        super(message);
+public class ReviewSessionException extends BusinessException {
+
+    private ReviewSessionException(ErrorCode errorCode) {
+        super(errorCode);
     }
 
-    public ReviewSessionException(String message, Throwable cause) {
-        super(message, cause);
+    private ReviewSessionException(ErrorCode errorCode, String detail) {
+        super(errorCode, detail);
+    }
+
+    public static ReviewSessionException of(ErrorCode errorCode) {
+        return new ReviewSessionException(errorCode);
+    }
+
+    public static ReviewSessionException of(ErrorCode errorCode, String detail) {
+        return new ReviewSessionException(errorCode, detail);
     }
 }
 
